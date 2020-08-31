53fa16eacc5db6925cedeb2aae7c6b4f8133abda
kaustubh jaiswal <kaustubhjaiswal221@gmail.com>
2020-08-31 13:02:15 +0530

    second

--- app.py
+++ app.py
@@ -1,79 +1,116 @@
-from flask import Flask ,request , jsonify
+from flask import Flask , jsonify , request 
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-
+import os 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres+psycopg2://postgres:7898994162k@localhost:5432/augDB'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://bzjhedrrusadyw:c564f989468c53fd818d5ac36b9d6d9a7015cd04baf05d06839de28effa479b6@ec2-54-234-28-165.compute-1.amazonaws.com:5432/da1hn9t83vuk83'
+
+ 
 db = SQLAlchemy(app)
-CORS(app)
-
-
-class Contact(db.Model):
-  __tablename__ = "Contact"
-  id = db.Column(db.Integer, primary_key=True)
-  FirstName = db.Column(db.String)
-  LastName = db.Column(db.String)
-  Email = db.Column(db.String)
-  Phone = db.Column(db.BigInteger)
-
-  def __init__(self,FirstName,LastName,Email,Phone):
-      self.FirstName  = FirstName
-      self.LastName = LastName
-      self.Email = Email
-      self.Phone = Phone
-
-
-
-
-
-
-
-@app.route('/contact')
-def getContact():
-    con_List =  Contact.query.all()
-    conArray= []
-    for con in con_List :
-        c = {}
-        c['FirstName'] = con.FirstName
-        c['LastName'] = con.LastName
-        c['Email'] = con.Email
-        c['Phone'] = con.Phone
-        conArray.append(c)
-    return  jsonify(contactlist = conArray) 
-
-
-
-
-@app.route('/contact', methods=['POST'] )
-def postContact():
-    data = request.get_json()
-    FirstName = data['FirstName']
-    LastName = data['LastName']
-    Email = data['Email']
-    Phone = data['Phone']
-    cs = Contact(FirstName,LastName,Email,Phone)
-    db.session.add(cs)
-    db.session.commit()
-    return  jsonify('data is posted Successfully') 
-
-
-
-
-
-
-
-@app.route('/test/<name>')
-def Hello_Alean(name):
-    return 'Hello ' +name +'!' 
-
-
-
-@app.route('/')
-def Hello_Alean2():
-    return render_template('index.html')
-
-
-if __name__ == "__main__":
-    db.create_all()
-    app.run(debug=True)
+CORS(app)  
+ 
+ 
+class Tasklists(db.Model):
+   __tablename__ = "tasklists"
+   id = db.Column(db.Integer, primary_key = True)
+   name = db.Column(db.String)    
+   
+#constructor function of  tasklits 
+   def __init__(self, name):
+    self.name = name
+    print("data created")
+ 
+ 
+ 
+#to create tasks tabel in database 
+class Tasks(db.Model):
+   __tablename__ = "tasks"
+   id = db.Column(db.Integer, primary_key = True)
+   text = db.Column(db.String)
+   Completed = db.Column(db.Boolean)
+   list_id = db.Column(db.Integer)
+ 
+   
+#constructor function of  tasklits 
+   def __init__(self, text ,list_id):
+    self.text = text
+    self.list_id = list_id
+    self.Completed = False
     
+  
+ 
+#to get the value of  tasklist 
+@app.route('/tasklists')  
+def get_taskList():  
+      task_lists = Tasklists.query.all()
+      task_lists_list =[] 
+      for task_list in task_lists:
+        t={}
+        t["id"] = task_list.id
+        t["name"] = task_list.name    
+        task_lists_list.append(t)
+ 
+      return jsonify(tasklist=task_lists_list) 
+ 
+
+# post thet tasklist
+@app.route('/tasklists' ,  methods = ['POST']) 
+def create_task_lists():
+   data = request.get_json()
+   name = data["name"]
+   ts = Tasklists(name)   
+   db.session.add(ts)
+   db.session.commit()
+   return jsonify(message = " task list is created") ,201
+
+
+# get the task 
+@app.route('/tasks')
+def get_tasks():
+   task_lists_id  = request.args.get('task_list')
+   tasks = Tasks.query.filter_by(list_id = task_lists_id)
+   task_list = []
+   for task in tasks:
+    t={}
+    t["id"] = task.id 
+    t["text"] = task.text
+    t["Completed"]  = task.Completed
+    t["task_id"] = task.list_id
+    task_list.append(t)
+   return jsonify(task = task_list)  
+# post a new task 
+
+@app.route('/tasks', methods = ['POST'])  
+def create_task():
+   data = request.get_json()
+   text = data["text"]
+   list_id = data["list_id"]   
+   tl =  Tasks(text,list_id) 
+   db.session.add(tl)
+   db.session.commit()
+   return jsonify(message ="task  created") , 201 
+
+# put the  task or update the task 
+@app.route('/tasks/<id>', methods = ['PUT'])
+def update_task(id):   
+   task = Tasks.query.filter_by (id = id).first()
+   task.Completed = not(task.Completed)
+   db.session.add(task)
+   db.session.commit()
+   return jsonify(message = "task updated" , task_id = task.id, task_Completed = task.Completed)
+
+
+# delete  the task 
+@app.route("/tasks/<id>" , methods = ['DELETE'])
+def delete(id):
+   task =  Tasks.query.filter_by (id = id).first() 
+   db.session.delete(task)
+   db.session.commit()
+   return jsonify(message = "delete task ")   
+ 
+@app.route('/')
+def get_task_lists():   
+ return "hello world"
+ 
+if __name__ == '__main__':
+   db.create_all()
